c86ab2dd53343351108fbe6e5a00cd1403003c9b
IIvexII <mrh4ck3r1738@gmail.com>
2022-08-24 11:10:01 +0500

    Fixed typo

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,9 +1,9 @@
 import { mongoose } from '../config/mongoose';
 
-const UserSchema = new mongoose.Schema({
+const StudentSchema = new mongoose.Schema({
   rollNumber: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-export const Student = mongoose.model('Student', UserSchema);
+export const Student = mongoose.model('Student', StudentSchema);
